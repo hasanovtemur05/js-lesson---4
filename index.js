7b320db81e9197668516e374b93e3ec0a1a1d3a1
hasanovtemur05 <hasanovtemur86@gmail.com>
2024-06-29 22:53:59 +0500

    added about

--- index.js
+++ index.js
@@ -21,8 +21,44 @@
 
 
 // 3 - masala
-const  teskarison = (son) => {
-    let son1 = son
-    console.log(son % 10,parseInt(son / 10));
-}
-teskarison(99) 
+// const  teskarison = (son) => {   
+//     if (son > 9 && son < 100) {
+//         console.log("ikki xonali son kiritdingiz");
+//         console.log(son % 10,parseInt(son / 10 ), "teskari son");
+//     }else {
+//         console.log("siz ikki xonali son kiritmadingiz");
+//     }
+// }
+// teskarison(10) 
+
+
+
+// 4 - masala
+// const  teskarison = (son) => {   
+//     if (son > 99 && son < 1000) {
+//         console.log("uch xonali son kiritdingiz");
+//         console.log(son % 100 % 10, parseInt(son % 100 / 10),parseInt(son / 100 ), "teskari son");
+//     }else {
+//         console.log("siz uch xonali son kiritmadingiz");
+//     }
+// }
+// teskarison() 
+
+
+
+// 5 - masala
+// function student(ball) {
+//     if (ball <= 100 && ball >= 70) {
+//         return "budjet"
+//     }else if (ball < 70 && ball > 50) {
+//         return "contrakt"
+//     }else {
+//        return "yiqildi"
+//     }
+// }
+
+// console.log(student());
+
+
+
+// 6 - masala

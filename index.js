6686b53e184740b9584eec9740d2567c8d6b0885
hasanovtemur05 <hasanovtemur86@gmail.com>
2024-06-29 21:58:13 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+// 1 - Masala sodda kalkulator yaratish
+    // let number1 = parseFloat(prompt("son kiriting"))
+    // let number2 = parseFloat(prompt("ikkinchi sonni kiriting"))
+    // console.log(number1);
+    // console.log(number2);
+    // console.log(number1 / number2 , "bo'linma");
+    // console.log(number1 * number2 , "kupaytma");
+    // console.log(number1 - number2 , "ayirma");  
+    // console.log(number1 + number2 , "yig'indi");
+
+
+
+// 2 - msasla 
+// let son = parseFloat(prompt("son kiriting"))
+// if (son > 0) {
+//     console.log(son + 1);
+// }else {
+//     console.log(son - 1);
+// }
+
+
+
+// 3 - masala
+const  teskarison = (son) => {
+    let son1 = son
+    console.log(son % 10,parseInt(son / 10));
+}
+teskarison(99) 
